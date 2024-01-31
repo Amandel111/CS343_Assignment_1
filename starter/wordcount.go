85f416d57be5dea84fe06f36362c921c782c8475
Anabelle Mandel <anabelle.world@yahoo.com>
2024-01-30 19:56:49 -0500

    started multi threading

--- starter/wordcount.go
+++ starter/wordcount.go
@@ -3,15 +3,17 @@ package main
 import (
 		"fmt"
 		"os"
-		//"log"
+		"log"
 		//"io/ioutil"
 		"strings"
 		"regexp"
 		"strconv"
 	)
 
-var numWords = make(map[string]int)
-func createDict (word string){
+//global variables
+var numWordSingle = make(map[string]int)
+var numWordDouble = make(map[string]int)
+func createDict (word string, dict map[string] int){
 	if word != ""{
 		lowercaseWord := strings.ToLower(word)
 		allLetterWord := ""
@@ -22,20 +24,12 @@ func createDict (word string){
 				//fmt.Print(" ", string(lowercaseWord[i]), " is a character.")
 			}
 		}
-		//fmt.Print("only letters ", allLetterWord)
-		//fmt.Print("lowercaseWord", lowercaseWord)
-		numWords[allLetterWord] += 1
-		//fmt.Print(lowercaseWord, numWords[lowercaseWord])
+		dict[allLetterWord] += 1
 	}
 }
 func single_threaded(files []string) {
-	// TODO: Your single-threaded implementation
-	//for each element in files, if the word is separated by a space/pubctuation : if it exists, increment dictionary, othewise add to dictionary
-	//need to loop thorugh each string in files
-	//for each string, figure out how to access txt file connected to the string (as a filepath)
-	//numWords := make(map[string]int)
-	fmt.Print(files[0])
-	//content, err:= os.ReadFile(files[0])
+
+	//read from files and pass to diciotnary creator 
 	for i := 0; i < len(files); i++{
 		content, err:= os.ReadFile(files[i])
 		if err != nil{
@@ -47,32 +41,69 @@ func single_threaded(files []string) {
 		wordList := re1.Split(stringContent, -1)
 		fmt.Printf("wordList W", wordList)
 		for j := 0; j < len(wordList); j++{
-			createDict(wordList[j])
+			createDict(wordList[j], numWordSingle)
 		}
-	file, err := os.Create("output/single.txt")
+	}
+	write_to_file("output/single.txt", numWordSingle)
+
+
+}
+
+func write_to_file(filepath string, dict map[string] int){
+	file, err := os.Create(filepath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for key, value := range numWords{
+	for key, value := range dict{
 		s := key + ": " + strconv.FormatInt(int64(value), 10) + "\n"
 		file.WriteString(s)
 	}
 	
-	fmt.Print(numWords)
-	//fmt.Print(output);
-	//fmt.Print(string(content));
-	//return content
 }
-}
+
+
 
 func multi_threaded(files []string) {
 	// TODO: Your multi-threaded implementation
+	const NUM_THREADS = 2;
+	//dictionary for words as a shared resource, will have mutual exclusion via lock/unlock
+	//we have the number of threads we want, NUM_THREADS
+	//we loop thorugh file
+	//we have a file, split it into NUM_THREADS number of files/strings that we pass
+	//we call the goroutine thread Num_THREADS number of times, this fills in the diciotnary
+	for i := 0; i < len(files); i++{
+
+		//divide file based on length
+		file, err := os.Open( files[i]) 
+	if err != nil {
+    	log.Fatal(err)
+	}
+	fi, err := file.Stat()
+	if err != nil {
+    	log.Fatal(err)
+	}
+	fmt.Println( "file size" , fi.Size() )
+	sizeOFileChunk := fi.Size() / NUM_THREADS 
+	fmt.Print("size of chunk", sizeOFileChunk)
+		/*content, err:= os.ReadFile(files[i])
+		if err != nil{
+			panic(err)
+		}
+		stringContent := string(content)
+		re1 := regexp.MustCompile(`\p{P}|[^\S+]`)
+	
+		wordList := re1.Split(stringContent, -1)
+		fmt.Print(wordList)
+	}
+*/
+}
 }
 
 
 func main() {
 	// TODO: add argument processing and run both single-threaded and multi-threaded functions
-	single_threaded([]string{"input/book.txt", "input/book2.txt"});
+	single_threaded([]string{"input/book.txt", "input/book2.txt"})
+	multi_threaded([]string{"input/book.txt", "input/book2.txt"})
 }
